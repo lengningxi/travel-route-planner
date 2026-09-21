eb915f06f99acd6058a11f51c83729c12547c69f
万维 <142858371+lengningxi@users.noreply.github.com>
2026-09-21 17:04:38 +0800

    feat: use real transit API (v5) for bus/subway routes in both web and miniprogram

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -2,6 +2,28 @@ const citiesData = require('../../data/cities');
 
 const HOT_CITIES = ['北京','上海','广州','深圳','成都','重庆','杭州','西安','南京','武汉','长沙','厦门','青岛','大理','丽江','三亚','哈尔滨','桂林','拉萨','苏州'];
 const AMAP_KEY = '222fa2feda1bb273c00c8f547b27a91c';
+const AMAP_WEB_SERVICE_KEY = '1f54fdca3afee4b40ce411b9985bbf55';
+
+const CITY_ADCODE_MAP = {
+  '北京':'110000','上海':'310000','广州':'440100','深圳':'440300','成都':'510100',
+  '重庆':'500000','杭州':'330100','西安':'610100','南京':'320100','武汉':'420100',
+  '长沙':'430100','厦门':'350200','青岛':'370200','大理':'532900','丽江':'530700',
+  '三亚':'460200','哈尔滨':'230100','桂林':'450300','拉萨':'540100','苏州':'320500',
+  '天津':'120000','昆明':'530100','洛阳':'410300','张家界':'430800','黄山':'341000',
+  '贵阳':'520100','开封':'410200','郑州':'410100','济南':'370100','沈阳':'210100',
+  '大连':'210200','长春':'220100','福州':'350100','合肥':'340100','南昌':'360100',
+  '太原':'140100','石家庄':'130100','呼和浩特':'150100','南宁':'450100','海口':'460100',
+  '兰州':'620100','银川':'640100','西宁':'630100','乌鲁木齐':'650100','珠海':'440400',
+  '无锡':'320200','佛山':'440600','东莞':'441900','宁波':'330200','温州':'330300'
+};
+
+function getCityAdcode(city) {
+  if (CITY_ADCODE_MAP[city]) return CITY_ADCODE_MAP[city];
+  for (const [name, code] of Object.entries(CITY_ADCODE_MAP)) {
+    if (city && city.includes(name)) return code;
+  }
+  return '110000';
+}
 
 const PINYIN_MAP = {};
 function buildPinyinMap() {
@@ -84,23 +106,59 @@ function searchWalking(origin, dest) {
 
 function searchTransit(origin, dest, city) {
   return new Promise((resolve) => {
-    searchDriving(origin, dest).then(driving => {
-      let transitDistance, transitTime;
-      if (driving && driving.distance && driving.time) {
-        transitTime = Math.round(driving.time * 1.8);
-        transitDistance = driving.distance;
-      } else {
-        const haversineM = haversine(origin.lng, origin.lat, dest.lng, dest.lat);
-        transitDistance = Math.round(haversineM * 1.3);
-        transitTime = Math.round(transitDistance / 500 * 60);
+    const oLng = origin.getLng ? origin.getLng() : origin.lng;
+    const oLat = origin.getLat ? origin.getLat() : origin.lat;
+    const dLng = dest.getLng ? dest.getLng() : dest.lng;
+    const dLat = dest.getLat ? dest.getLat() : dest.lat;
+    const adcode = getCityAdcode(city);
+    const url = `https://restapi.amap.com/v5/direction/transit/integrated?origin=${oLng},${oLat}&destination=${dLng},${dLat}&city1=${adcode}&city2=${adcode}&key=${AMAP_WEB_SERVICE_KEY}`;
+
+    wx.request({
+      url: url,
+      success(res) {
+        const data = res.data;
+        if (data.status === '1' && data.route && data.route.transits && data.route.transits.length > 0) {
+          const transit = data.route.transits[0];
+          const steps = [];
+          const segments = transit.segments || [];
+          segments.forEach(seg => {
+            if (seg.walking && parseInt(seg.walking.distance) > 0) {
+              steps.push({
+                transit_mode: 'walking',
+                distance: parseInt(seg.walking.distance),
+                duration: Math.round(parseInt(seg.walking.distance) / 80)
+              });
+            }
+            if (seg.bus && seg.bus.buslines && seg.bus.buslines.length > 0) {
+              const line = seg.bus.buslines[0];
+              const isMetro = line.type && line.type.includes('地铁');
+              steps.push({
+                transit_mode: isMetro ? 'subway' : 'bus',
+                line_name: line.name || (isMetro ? '地铁' : '公交'),
+                departure_stop: line.departure_stop ? line.departure_stop.name : '',
+                arrival_stop: line.arrival_stop ? line.arrival_stop.name : '',
+                via_num: line.via_num ? parseInt(line.via_num) : 0
+              });
+            }
+            if (seg.railway && seg.railway.lines && seg.railway.lines.length > 0) {
+              const line = seg.railway.lines[0];
+              steps.push({
+                transit_mode: 'train',
+                line_name: line.name || '火车',
+                departure_stop: line.departure_stop ? line.departure_stop.name : '',
+                arrival_stop: line.arrival_stop ? line.arrival_stop.name : ''
+              });
+            }
+          });
+          const totalDist = parseInt(transit.distance) || parseInt(data.route.distance);
+          resolve({ steps, distance: totalDist, time: 0, _estimated: false });
+        } else {
+          resolve(null);
+        }
+      },
+      fail() {
+        resolve(null);
       }
-      const busLineName = `公交 ${Math.floor(Math.random() * 900 + 100)}路`;
-      resolve({
-        distance: transitDistance,
-        time: transitTime,
-        _estimated: true,
-        line_name: busLineName
-      });
     });
   });
 }
